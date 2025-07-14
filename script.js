460c53d0086db6774bc583b9ff208d4ed4ca598f
Saranya Barani <62207142+Saranya-git-20@users.noreply.github.com>
2025-07-14 21:16:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function convertToRoman(num) {
 
   let result = "";
 
-	for (const { value, symbol } of romanMap) {
+	for (const { value, symbol } of obj) {
 		while (num >= value) {
 			result += symbol;
 			num -= value;
@@ -23,7 +23,7 @@ function convertToRoman(num) {
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-console.log(convertToRoman(36));
+// console.log(convertToRoman(36));
 
 
 
